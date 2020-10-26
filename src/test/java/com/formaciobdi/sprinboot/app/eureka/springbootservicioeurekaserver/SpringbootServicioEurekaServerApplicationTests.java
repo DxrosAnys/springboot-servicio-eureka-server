4b49197db749c405d5f1aa48a0e9c7f9a41c834d
DxrosAnys <diego.267.ri@gmail.com>
2020-10-25 19:49:53 -0500

    servicio eureka server

---
+++ src/test/java/com/formaciobdi/sprinboot/app/eureka/springbootservicioeurekaserver/SpringbootServicioEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formaciobdi.sprinboot.app.eureka.springbootservicioeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
